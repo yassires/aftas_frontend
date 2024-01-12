ca780c5d4b3a553f10a831a373d11d70e86486af
ysres <yas.esraidi@gmail.com>
2024-01-12 17:30:25 +0100

    add dockerFile and docker config

--- aftas-frontend/src/app/app-routing.module.ts
+++ aftas-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { CompetitionComponent } from './components/competition/competition.component';
+import { CompetitionFormComponent } from './components/competition/competition-form/competition-form.component';
+import { MemberComponent } from './components/member/member.component';
+import { MemberFormComponent } from './components/member/member-form/member-form.component';
+import { CompetitionMemberFormComponent } from './components/competition/competition-member-form/competition-member-form.component';
+import { RankingComponent } from './components/ranking/ranking.component';
+import { HuntingComponent } from './components/hunting/hunting.component';
+import { HuntingFormComponent } from './components/hunting/hunting-form/hunting-form.component';
+import { RankingTopComponent } from './components/ranking/ranking-top/ranking-top.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path:'competition-form', component: CompetitionFormComponent },
+  { path: 'member-form', component: MemberFormComponent },
+  { path: 'competitions', component: CompetitionComponent },
+  { path: 'members', component: MemberComponent },
+  { path: 'rankings', component:RankingComponent },
+  { path:'hunting-form', component: HuntingFormComponent },
+  { path: 'huntings', component: HuntingComponent },
+  { path: 'ranking-top', component: RankingTopComponent },
+  { path: 'competition-member-form', component: CompetitionMemberFormComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
